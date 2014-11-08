afee6df658aa57604356e36bec9bf4a1e3e887cb
Addisu Z. Taddese <addisu@gmail.com>
2014-11-08 17:48:38 -0600

    Implement gotcha post request

--- app/controllers/api/v1/report_dogs_controller.rb
+++ app/controllers/api/v1/report_dogs_controller.rb
@@ -6,10 +6,17 @@ class Api::V1::ReportDogsController < ApplicationController
   # Reporting that a dog has been seen by an app
   def gotcha
     args = JSON.parse(request.body.string)
-    thing = Thing.where(:uuid => args[:thing_id]).first
-    thing.latitude = args[:latitude]
-    thing.longitude = args[:longitude]
-    thing.precision = args[:precision]
-    thing.save
+    thing = Thing.where(:uuid => args["observedID"]).first
+
+    unless thing.nil?
+      thing.latitude = args[:latitude]
+      thing.longitude = args[:longitude]
+      thing.precision = args[:precision]
+      thing.save
+      render :json => {:status => "SUCCESS"}
+    else
+      render :json => {:status => "ERROR"}
+    end
+    #puts("Got message: ", request.body.string)
   end
 end
