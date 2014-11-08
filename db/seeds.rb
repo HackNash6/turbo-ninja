f025343920762a89c3d55408bc64b444dc96d06d
Brian Broll <brian.broll@gmail.com>
2014-11-08 17:38:30 -0600

    adsf

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 ThingType.delete_all
-ThingType.create(name: 'Dog')
+_dog = ThingType.create(name: 'Dog')
 _cat = ThingType.create(name: 'Cat')
 
 # Developing Stuff
 user = User.create(email: 'dev@dev.com', password: 'password', password_confirmation: 'password');
-cat = Thing.create(name: 'Maui')
+cat = Thing.create(name: 'Einstein', uuid: '1235-abcd' )
+dog = Thing.create(name: 'Maui', uuid: '1234-abcd' )
 cat.thing_type = _cat
 user.things << cat
+dog.thing_type = _dog
+user.things << dog
