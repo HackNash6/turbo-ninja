da6c02ce662b5931cce41d5b3757790d4fa0f971
Brian Broll <brian.broll@gmail.com>
2014-11-09 08:53:29 -0600

    Added drop down overlay thing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
 //= require_tree .
 //= require underscore
 //= require jquery.purr
 //= require best_in_place
+
+$(function(){ $(document).foundation(); });
